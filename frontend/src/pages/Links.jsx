b8dd994a2dfd044a45d13ae6a1c0282dfc676413
rohitsuvedhan <rohitsuvedhan@natalnine.com>
2025-03-09 13:41:17 +0530

    Add Server Url

--- frontend/src/pages/Links.jsx
+++ frontend/src/pages/Links.jsx
@@ -1,7 +1,7 @@
 
 const Links = {
     //Backend Server Link - Connect with Backend + DB
-    server_Link: "http://localhost:5555/books"
+    server_Link: "https://templeofletters-server.onrender.com/books"
   };
 
 export default Links
